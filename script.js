513e25927f49bd9463a01acddd4c2ad140f78ba3
szzzzzzzz098 <szelintan77@gmail.com>
2025-02-09 00:23:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
- document.querySelector(".play-btn").addEventListener("click", function() {
+// Get the current page URL
+        let currentPage = window.location.pathname.split("/").pop();
+
+        // Find all navigation links
+        let navLinks = document.querySelectorAll(".nav-link");
+
+        // Loop through links & check if href matches the current page
+        navLinks.forEach(link => {
+            if (link.getAttribute("href") === currentPage) {
+                link.classList.add("active"); // Add "active" class
+            }
+        });
+
+document.querySelector(".play-btn").addEventListener("click", function() {
             window.open("https://youtu.be/8pDO2vBnehc?si=ts7Gbr4OSRPSwg05");
   alert("Playing Video...");
 });
