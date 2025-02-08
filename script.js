53e73c3bfe2db34dc5dbbbf08264c00cb64f630e
szzzzzzzz098 <szelintan77@gmail.com>
2025-02-09 00:44:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Get the current page URL
-        let currentPage = window.location.pathname.split("/").pop();
+let currentPage = window.location.pathname.split("/").pop();
 
-        // Find all navigation links
-        let navLinks = document.querySelectorAll(".nav-link");
+// Find all navigation links
+let navLinks = document.querySelectorAll(".nav-link");
 
-        // Loop through links & check if href matches the current page
-        navLinks.forEach(link => {
-            if (link.getAttribute("href") === currentPage) {
-                link.classList.add("active"); // Add "active" class
-            }
-        });
+// Loop through links & check if href matches the current page
+navLinks.forEach(link => {
+    if (link.getAttribute("href") === currentPage) {
+        link.classList.add("active"); // Add "active" class
+    }
+});
 
 document.querySelector(".play-btn").addEventListener("click", function() {
             window.open("https://youtu.be/8pDO2vBnehc?si=ts7Gbr4OSRPSwg05");
